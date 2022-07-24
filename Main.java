593cdba4e67ffd978a528c1a822521b097a8aa93
Emin <emirkara21@gmail.com>
2022-07-24 20:14:36 +0300

    Son hali

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
-
 public class Main{
     public static void main(String[] args){
-
     double a, r, p = 3.14, alan, cevre, aciAlan;
   
     System.out.println("Dairenin Yarıçapını Girin: ");
     Scanner girdi = new Scanner(System.in);
     r = girdi.nextDouble(); 
+    
     System.out.println("Dairenin İstenen Açısını Girin: ");
     Scanner girdi2 = new Scanner(System.in);
     a = girdi2.nextDouble(); 
@@ -16,15 +15,8 @@ public class Main{
     cevre = 2*p*r;
     aciAlan = (p*(r*r)*a)/360;
 
-
-
-
     System.out.println("Alan: "+alan+" Birim");
     System.out.println("Çevre: "+cevre+" Birim");
     System.out.println("Açıdaki Yayın Alanı: "+aciAlan+" Birim");
-
-
-
-
     } 
 }

3bc24d0657f3339381e16fb66e06130e79da69fe
Emin <emirkara21@gmail.com>
2022-07-24 20:17:14 +0300

    Son Hali

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main{
     public static void main(String[] args){
     double a, r, p = 3.14, alan, cevre, aciAlan;
   
-    System.out.println("Dairenin Yarıçapını Girin: ");
+    System.out.print("Dairenin Yarıçapını Girin: ");
     Scanner girdi = new Scanner(System.in);
     r = girdi.nextDouble(); 
-    
-    System.out.println("Dairenin İstenen Açısını Girin: ");
+
+    System.out.print("Dairenin İstenen Açısını Girin: ");
     Scanner girdi2 = new Scanner(System.in);
     a = girdi2.nextDouble(); 
     
